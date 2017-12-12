4d54a49112fc6575057968bc7bb6ab85abc5f3f3
André Minoro Fusioka <minorofusioka@hotmail.com>
2017-12-11 22:38:37 -0200

    Ajuste das pastas no namespace correto

---
+++ tests/CDC/Loja/Produto/MaiorEMenorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace CDC\Loja\Produto;
+
+require "./vendor/autoload.php";
+
+use CDC\Loja\Carrinho\CarrinhoDeCompras;
+use CDC\Loja\Produto\MaiorEMenor;
+use CDC\Loja\Produto\Produto;
+
+use	PHPUnit_Framework_TestCase as PHPUnit;
+
+class MaiorEMenorTest extends PHPUnit{
+	
+	public function testOrdemDecrescente(){
+
+		$carrinho = new CarrinhoDeCompras();
+
+		$carrinho->adiciona(new Produto("Geladeira", 450.00));
+		$carrinho->adiciona(new Produto("Liquidificador", 250.00));
+		$carrinho->adiciona(new Produto("Jogo de Pratos", 70.00));
+
+		$maiorMenor = new MaiorEMenor();
+		$maiorMenor->encontra($carrinho);
+
+		$this->assertEquals("Jogo de Pratos", $maiorMenor->getMenor()->getNome());
+		$this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+	}
+
+	public function testApenasUmProduto(){
+		$carrinho = new CarrinhoDeCompras();
+
+		$carrinho->adiciona(new Produto("Geladeira", 450.00));
+
+		$maiorMenor = new MaiorEMenor();
+		$maiorMenor->encontra($carrinho);
+
+		$this->assertEquals("Geladeira", $maiorMenor->getMenor()->getNome());
+		$this->assertEquals("Geladeira", $maiorMenor->getMaior()->getNome());
+
+	}
+
+}
+
+?>
