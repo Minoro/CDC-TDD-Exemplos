3c260791ff33d3823dc78c204ffb69cc8b8f7e39
André Minoro Fusioka <minorofusioka@hotmail.com>
2017-12-26 22:43:57 -0200

    Exemplo de testes de acoplamento com NF

---
+++ src/CDC/Loja/FluxoDeCaixa/GeradorDeNotaFiscal.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CDC\Loja\FluxoDeCaixa;
+
+use CDC\Loja\FluxoDeCaixa\NotaFiscal;
+use CDC\Loja\FluxoDeCaixa\Pedido;
+
+class GeradorDeNotaFiscal{
+	
+	public function gera(Pedido $pedido){
+		return new NotaFiscal($pedido->getCliente(), $pedido->getValorTotal() * 0.94, new \DateTime());
+	}
+}
+
+?>
